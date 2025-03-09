06b7c1247eb80599dd9460d1e3256f88fdd5ee38
brvishwa-labs <vishwa.labs@gmail.com>
2025-03-09 12:02:20 +0530

    day2 completed

---
+++ JAVASCRIPT/day2.js
@@ -0,0 +1,15 @@
+let  a=24;
+console.log(a);
+console.log(typeof(a));
+let b=3.14;
+console.log(b);
+console.log(typeof(b));
+let z=1234567894546546465465464646n;
+console.log(z);
+console.log(typeof(z));
+let c="hello";
+console.log(c);
+console.log(typeof(c));
+let d=true;
+console.log(d);
+console.log(typeof(d));
